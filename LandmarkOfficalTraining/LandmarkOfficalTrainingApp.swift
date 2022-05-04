1ee06bf19aeb20ca6599448cb00dc7659314a9b3
ZhangPluto <lionelzhang99@gmail.com>
2022-05-03 22:01:34 -0400

    Initial Commit

---
+++ LandmarkOfficalTraining/LandmarkOfficalTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkOfficalTrainingApp.swift
+//  LandmarkOfficalTraining
+//
+//  Created by Renhao Zhang on 2022-05-03.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkOfficalTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
